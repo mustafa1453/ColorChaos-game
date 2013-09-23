4db9843dc99df47e37f97ac16af15e5f6c41e07a
Alex Smolyankin <as@web.drucode.com>
2013-09-23 22:53:53 +0300

    Made workable version.

--- js/custom/game.js
+++ js/custom/game.js
@@ -1,71 +1,65 @@
 /**
  * Created by mustafa1453 on 9/22/13.
  */
-var colors = ["red", "blue", "orange", "violet", "green", "yellow"];
 $(function() {
+    Object.prototype.clone = function() {
+        var newObj = (this instanceof Array) ? [] : {};
+        for (i in this) {
+            if (i == 'clone') continue;
+            if (this[i] && typeof this[i] == "object") {
+                newObj[i] = this[i].clone();
+            } else newObj[i] = this[i]
+        } return newObj;
+    };
+
+    var colors = ["red", "blue", "orange", "violet", "green", "yellow"];
     var gameColor = $(".game .color");
     var chooser = $(".colors .color");
     var gameArray = new Array();
-    var tmpArray = new Array();
     var iMax = 5;
     for (var i = 0; i < iMax; i++) {
         gameArray[i] = new Array();
-        tmpArray[i] = new Array();
     }
+
     gameColor.each(function(index, element) {
         var color = Math.floor(Math.random() * 6);
         gameArray[Math.floor(index / 5)][index % 5] = color;
         $(this).addClass(colors[color]);
     });
+
     chooser.click(function() {
         var oldColor = $(gameColor[0]).attr("class");
+        var colorIndex = colors.indexOf(oldColor.replace("color ", ""));
         var color = $(this).attr("class");
-        // Loop all boxes with the same color, and find if they are neighborhood for first colorbox.
-        tmpArray = gameArray;
+        var newColorIndex = colors.indexOf(color.replace("color ", ""));
+        var tmpArray = gameArray.clone();
         gameColor.each(function(index, element) {
             console.log(index);
             if ($(this).attr("class") === oldColor) {
                 var x = Math.floor(index / 5);
                 var y = index % 5;
-                var cleanColor = oldColor.replace("color ", "");
-                var colorIndex = colors.indexOf(cleanColor);
-                if (isNear(gameArray, x, y, colorIndex)) {
-                    tmpArray[x][y] = colorIndex;
+                if (isNear(gameArray.clone(), x, y, colorIndex)) {
+                    tmpArray[x][y] = newColorIndex;
                     $(this).removeClass().addClass(color);
                 }
             }
         });
-        gameArray = tmpArray;
+        gameArray = tmpArray.clone();
     });
 
-
-    // Just test
-    /*gameArray[0][0] = 5;
-    gameArray[1][0] = 5;*/
-    /*if (isNear(gameArray, 1, 0, 5)) {
-        console.log("Path is find");
-    }*/
-
-    function isNear(grid, x, y, color) {
-        var isFind = false;
-        var easystar = new EasyStar.js();
-        easystar.setGrid(grid);
-        easystar.setAcceptableTiles([color]);
-        easystar.findPath(0, 0, x, y, function(path) {
-            if (path === null) {
-                isFind = false;
-            } else {
-                isFind = true;
-            }
+    function isNear(matrix, y, x, color) {
+        matrix.forEach(function(row, i) {
+            matrix[i] = row.map(function(item) { return item == color ? 0 : 1; });
         });
-        easystar.calculate();
-        return isFind;
+        var grid = new PF.Grid(matrix.length, matrix.length, matrix);
+        var finder = new PF.AStarFinder();
+        var path = finder.findPath(0, 0, x, y, grid);
+        return path.length > 0;
     }
 
-    // showMatrix(gameArray);
-    /*function showMatrix(matrix) {
+    function showMatrix(matrix) {
         for (row in matrix) {
             console.log(matrix[row]);
         }
-    }*/
+    }
 });
